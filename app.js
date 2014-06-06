09065506858580d309564e96ca150f1bc08b2101
Dominik Schlaepfer <doemsche@gmx.ch>
2014-06-06 09:57:09 +0200

    initial commit (move from blitzpoll-server to own repo)

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express'),
+	app = express(),
+	routes = require('./routes'),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	url = require('url'),
+	mongoose = require('mongoose'),
+	dbHost = process.env.DBHOST || 'mongodb://127.0.0.1/questions',
+	net = require('net');
+
+server.listen(5000);
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', routes.index);
+
+mongoose.connect(dbHost, function(err){
+	if(err){
+		console.log(err);
+	} else {
+		console.log('connected');
+	}
+});
+
+var questionSchema = {
+	question: String,
+	answers: Array,
+	time: Number,
+	created: { type: Date, default: Date.now } 
+}
+var Question = mongoose.model('Question', questionSchema);
+
+var client = net.connect({ path: '/tmp/hacksports.sock' }, function() {
+    console.log('got connection!');
+});
+
+
+
+io.sockets.on('connection', function(socket){
+	console.log('socket connection');
+	var query = Question.find({}, function(err,docs){
+		if(err) throw err
+		socket.emit('load old questions', docs);
+	});
+
+	socket.on('new question', function(data){
+		var newQuestion = new Question({
+			question: data.question,
+			answers: data.answers,
+			time: data.time
+		});
+		newQuestion.save(function(err){
+			if(err) throw err;
+			else
+				io.sockets.emit('new question added',{data:newQuestion})
+				client.write(JSON.stringify(newQuestion));
+		});
+	});
+});
---
+++ public/client.js
@@ -0,0 +1,79 @@
+jQuery(function($){
+	var socket = io.connect();
+	var question = $('#question');
+  var answers = $('#possible-answers');
+  var time = $('#time-frame');
+  var countdownindicator =$('#countdown-indicator');
+  var readyindicator = $('#ready-indicator');
+  var activequestion = $('#active-question');
+  countdownindicator.hide();
+  readyindicator.show();
+
+  var countdown = $('#countdown');
+  var count = 0;
+  var counter;
+
+  var resetForm = function(){
+    question.val('');
+    answers.val('');
+    time.val('');
+  };
+
+  var clearTimer = function(){
+    clearInterval(counter);
+  }
+  var activateTimer = function(data){
+
+    readyindicator.hide();
+    countdownindicator.show();
+    //offset to compensate latency:
+    var offset = 1;
+    count = parseInt(data.time)+ offset;
+    qText = data.question;
+    activequestion.text(qText);
+    counter=setInterval(timer, 1000);
+    function timer(){
+      count = count -1;
+      countdown.text(count);
+       if (count <= 0)
+        {
+           clearTimer();
+           //countdown.text('');
+           countdownindicator.hide();
+           readyindicator.show();
+           return;
+        }
+    }
+    //countdown.text(data.time);
+  };
+
+
+
+  $('#newquestion-form').submit(function(e){
+    e.preventDefault();
+    clearTimer();
+    var newQuestion = {
+      question: question.val(),
+      answers: answers.val().split(','),
+      time: time.val()
+    }
+    socket.emit('new question', newQuestion);  	
+  });
+
+  socket.on('new question added', function(data){
+    $('#q-container').append('<div>'+data.data.question+'</div>');
+    activateTimer(data.data);
+    resetForm();
+  });
+
+  socket.on('load old questions', function(docs){
+    for(var i= docs.length-1; i>0; i--){
+      displayQuestions(docs[i]);
+    }
+
+  });
+
+  function displayQuestions(data){
+    $('#q-container').append('<div>'+data.question+'</div>');
+  }
+})
